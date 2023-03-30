90141b0aaa1b39a884e1cfd6539801e056382008
garryling <linggaoyuan@gmail.com>
2023-03-30 08:17:39 +0800

    feat: add ti pyramid

--- tai_pyramid.py
+++ tai_pyramid.py
@@ -1,5 +1,14 @@
 import taichi as ti
 
+img2d = ti.types.ndarray(ti.math.vec3, ndim=2)
 
-@kernel
-def tai_pyramid(img: ):
+# same pyramid as opencv
+@ti.kernel
+def tai_pyramid(img: img2d, level: int, win_size: int):
+    # pad image with window size 
+    
+    pass
+
+
+if __name__ == "__main__":
+    tai_pyramid()
