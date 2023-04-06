76aace77e57e9e561be9a5cf553e3d7b73e405cb
garryling <linggaoyuan@gmail.com>
2023-04-07 01:05:23 +0800

    feat: add pyrdown

--- tai_pyramid.py
+++ tai_pyramid.py
@@ -3,6 +3,8 @@ import cv2 as  cv
 
 img_rgb3_2d = ti.types.ndarray(ti.math.vec3, ndim=2)
 img_gray_2d = ti.types.ndarray(ti.u8, ndim=2)
+ti.init(arch=ti.cuda)
+weight = ti.field(dtype=ti.f32, shape=5, offset=-2)
 
 def prepare(h, w, level):
     pyrs = []
@@ -20,20 +22,6 @@ def prepare(h, w, level):
 @ti.kernel
 def img_with_pad(img: img_gray_2d, img_pad: img_gray_2d, pad_size:int):
     h, w = img.shape
-    #for i, j in ti.ndrange(h + 2 * pad_size, w + 2 * pad_size):
-    #    if i >= pad_size and i < h + pad_size and j >= pad_size and j < w + pad_size:
-    #        img_pad[i, j] = img[i - pad_size, j - pad_size] 
-    #    elif i < pad_size:
-    #        img_pad[i, j] = img[0, j - pad_size]
-    #    elif j < pad_size:
-    #        img_pad[i, j] = img[i - pad_size, 0]
-    #    elif i >= pad_size + h:
-    #        img_pad[i, j] = img[h - 1, j - pad_size]
-    #        # img_pad[i, j] = 255
-    #    elif j >= pad_size + w:
-    #        img_pad[i, j] = img[i - pad_size, w - 1]
-    #        # img_pad[i, j] = 255
-
     for i, j in ti.ndrange(h, w):
         img_pad[i + pad_size, j + pad_size] = img[i, j]
     
@@ -53,18 +41,47 @@ def img_with_pad(img: img_gray_2d, img_pad: img_gray_2d, pad_size:int):
     for i in ti.ndrange((h + pad_size, h + 2* pad_size)):
         for j in ti.ndrange((0, pad_size * 2 + w)):
             img_pad[i, j] = img_pad[h - 1 + pad_size, j]
-        
 
-# same pyramid as opencv
-#@ti.kernel
-#def tai_pyramid(img: img2d, level: int, win_size: int):
-#    # pad image with window size 
-#    
-#    pass
+@ti.kernel
+def img_with_pyrdown(img: img_gray_2d, img_blur: img_gray_2d, img_down: img_gray_2d):
+    h, w = img.shape
+    weight[0] = 1
+    weight[1] = 4
+    weight[2] = 6
+    weight[3] = 4
+    weight[4] = 1
+    radius = 2
+    total_weight = 16
+    for i, j in ti.ndrange(h, w):
+        l_begin, l_end = ti.max(0, i - radius), ti.min(h, i + radius + 1)
+        total = 0.0
+        for l in range(l_begin, l_end):
+            wi= weight[l - i]
+            total += img[l, j] * wi
+        total /= total_weight
+        img_blur[i,j] = ti.cast(total, ti.u8)
+    
+    for i, j in ti.ndrange(h, w):
+        l_begin, l_end = ti.max(0, j - radius), ti.min(w, j + radius + 1)
+        total = 0.0
+        for l in range(l_begin, l_end):
+            wi = weight[l - j]
+            total += img_blur[i, l] * wi
+        total /= total_weight
+        img_blur[i,j] = ti.cast(total, ti.u8)
+
+    for i, j in ti.ndrange(h, w):
+        img_down[i, j] = (img[i * 2, j * 2] + img[i * 2 + 1, j * 2] + img[i * 2, j * 2 + 1] + img[i * 2 + 1, j * 2 + 1]) / 4.0
+
+
+@ti.kernel
+def tai_pyramid(img: img_gray_2d, level: int, win_size: int):
+    # pad image with window size 
+    pass
 
 
 if __name__ == "__main__":
-    ti.init(arch=ti.cuda)
+    # ti.init(arch=ti.cuda)
     img:img_gray_2d = cv.imread("./imgs/0.png", cv.IMREAD_GRAYSCALE)
     pad_size = 50
     h, w = img.shape
@@ -74,5 +91,10 @@ if __name__ == "__main__":
 
     img_pad_cv = cv.copyMakeBorder(img, 50, 50, 50, 50, cv.BORDER_REPLICATE)
     cv.imwrite("res_cv.pgm", img_pad_cv)
+
+    img_blur:img_gray_2d = ti.ndarray(ti.uint8, shape=(h + 2 * pad_size, w + 2 * pad_size))
+    img_res_div_2:img_gray_2d = ti.ndarray(ti.uint8, shape=(h // 2 + pad_size, w // 2 + pad_size))
+    img_with_pyrdown(img_res, img_blur,img_res_div_2)
+    cv.imwrite("res_div2.pgm", img_res_div_2.to_numpy())
     # pyrs = prepare(h, w, 3)
     # tai_pyramid()
