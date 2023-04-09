8c52815cbf5bab0122bca4a076bdca6a131b88f0
garryling <linggaoyuan@gmail.com>
2023-04-09 09:26:44 +0800

    fix: fix all bugs hint by taichi debug

--- tai_pyramid.py
+++ tai_pyramid.py
@@ -3,7 +3,7 @@ import cv2 as  cv
 
 img_rgb3_2d = ti.types.ndarray(ti.math.vec3, ndim=2)
 img_gray_2d = ti.types.ndarray(ti.u8, ndim=2)
-ti.init(arch=ti.cuda)
+ti.init(arch=ti.cpu, debug = True, print_kernel_llvm_ir_optimized=True)
 weight = ti.field(dtype=ti.f32, shape=5, offset=-2)
 
 def prepare(h, w, level):
@@ -45,11 +45,11 @@ def img_with_pad(img: img_gray_2d, img_pad: img_gray_2d, pad_size:int):
 @ti.kernel
 def img_with_pyrdown(img: img_gray_2d, img_blur: img_gray_2d, img_down: img_gray_2d):
     h, w = img.shape
-    weight[0] = 1
+    weight[-2] = 1
+    weight[-1] = 4
+    weight[0] = 6
     weight[1] = 4
-    weight[2] = 6
-    weight[3] = 4
-    weight[4] = 1
+    weight[2] = 1
     radius = 2
     total_weight = 16
     for i, j in ti.ndrange(h, w):
@@ -70,8 +70,14 @@ def img_with_pyrdown(img: img_gray_2d, img_blur: img_gray_2d, img_down: img_gray
         total /= total_weight
         img_blur[i,j] = ti.cast(total, ti.u8)
 
-    for i, j in ti.ndrange(h, w):
-        img_down[i, j] = (img[i * 2, j * 2] + img[i * 2 + 1, j * 2] + img[i * 2, j * 2 + 1] + img[i * 2 + 1, j * 2 + 1]) / 4.0
+    for i, j in ti.ndrange(h // 2, w // 2):
+        res = 0.0
+        res += img[i * 2, j * 2]
+        res += img[i * 2 + 1, j * 2] 
+        res += img[i * 2, j * 2 + 1] 
+        res += img[i * 2 + 1, j * 2 + 1]
+        
+        img_down[i, j] = ti.cast(res/4.0, ti.u8)
 
 
 @ti.kernel
@@ -96,5 +102,6 @@ if __name__ == "__main__":
     img_res_div_2:img_gray_2d = ti.ndarray(ti.uint8, shape=(h // 2 + pad_size, w // 2 + pad_size))
     img_with_pyrdown(img_res, img_blur,img_res_div_2)
     cv.imwrite("res_div2.pgm", img_res_div_2.to_numpy())
+    print("done")
     # pyrs = prepare(h, w, 3)
     # tai_pyramid()
